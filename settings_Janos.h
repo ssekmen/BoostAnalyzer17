04b315d2f0b7d3a7ec22db8af3a887a01973540a
chuh <changgi.huh@cern.ch>
2020-07-27 22:08:47 -0500

    setting*h

--- settings_Janos.h
+++ settings_Janos.h
@@ -7,8 +7,8 @@
 #endif
 
 #if SKIM == 0
-#include "Plotting_Janos.h" // Specify here the implementations for your Analysis
-//#include "Plotting_Changgi.h" // Specify here the implementations for your Analysis
+//#include "Plotting_Janos.h" // Specify here the implementations for your Analysis
+#include "Plotting_Changgi.h" // Specify here the implementations for your Analysis
 #else 
 #include "Analysis_Skim.h" // Specify here the implementations for your Analysis
 #endif
@@ -27,7 +27,8 @@ struct settings {
     doAK8JetPtRescaling      ( false  ),
     applySmearing            ( false  ),
     applyScaleFactors        ( true  ),
-    nSigmaScaleFactors       ( 34    ), // Count the number of sigmas you use in ScaleFactors.h
+    nSigmaScaleFactors       ( 64    ), // Count the number of sigmas you use in ScaleFactors.h
+    //nSigmaScaleFactors       ( 34    ), // Count the number of sigmas you use in ScaleFactors.h
     varySystematics          ( SYST  ),
     systematicsFileName      ( "systematics/2018_05_02_1SigmaUpDown_NoPdf.txt" ),
 //  systematicsFileName      ( "systematics/2017_12_26_AllUpDown_NoPdf.txt" ),
